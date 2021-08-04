4690554eb4ef64ec6c5877d00030273ef67509e1
wjuang <williamsjuang@gmail.com>
2021-08-03 20:47:06 -0400

    user can name tamagotchi

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+let userName = ""
+const requestName = () => {
+  userName = prompt("What would you like to name your Tamagotchi?")
+}
+
 class Tamagotchi {
   constructor(name){
     this.name = name
@@ -8,35 +13,43 @@ class Tamagotchi {
     this.horizontalPosition = 0
     this.verticalPosition = 0
   }
+  displayName(){
+    let topName = document.querySelector('h1')
+    topName.textContent = userName
+  }
+
   moveRight(){
     let pet = document.querySelector('#pet')
     let horizontal = pet.style.left.replace(/\D/g,'')
-    pet.style.left = (Number(horizontal) + 50) + "px"
+    pet.style.left = (Number(horizontal) + 10) + "px"
   }
   moveLeft(){
     let pet = document.querySelector('#pet')
     let horizontal = pet.style.left.replace(/\D/g,'')
-    pet.style.left = (Number(horizontal) - 50) + "px"
+    pet.style.left = (Number(horizontal) - 10) + "px"
   }
   moveUp(){
     let pet = document.querySelector('#pet')
     let vertical = pet.style.bottom.replace(/\D/g,'')
-    pet.style.bottom = (Number(vertical) + 50) + "px"
+    pet.style.bottom = (Number(vertical) + 10) + "px"
   }
   moveDown(){
     let pet = document.querySelector('#pet')
     let vertical = pet.style.bottom.replace(/\D/g,'')
-    pet.style.bottom = (Number(vertical) - 50) + "px"
+    pet.style.bottom = (Number(vertical) - 10) + "px"
+  }
+  resetPosition(){
+    let pet = document.querySelector('#pet')
+    pet.style.bottom = 0
+    pet.style.left = 0
   }
 }
 
-const first = new Tamagotchi('Test')
-first.moveRight()
-first.moveDown()
-first.moveDown()
-first.moveLeft()
-first.moveLeft()
-first.moveUp()
+
+//FUNCTIONS TO LOAD ON PAGE START
+requestName()
+const first = new Tamagotchi(userName)
+first.displayName()
 
 //WORKING MOVE RIGHT COMMAND
 // const moveRight = () => {
