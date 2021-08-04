d2c5822d68d0d58c1a0d0a149b597f2c24d56421
wjuang <williamsjuang@gmail.com>
2021-08-03 20:33:02 -0400

    created move commands for all directions and tested

--- js/app.js
+++ js/app.js
@@ -2,23 +2,45 @@ class Tamagotchi {
   constructor(name){
     this.name = name
     this.age = 0
-    this.hunger = 100
-    this.sleep = 100
+    this.hunger = 10
+    this.sleep = 10
+    this.boredom = 10
+    this.horizontalPosition = 0
+    this.verticalPosition = 0
   }
   moveRight(){
     let pet = document.querySelector('#pet')
-    pet.style.left = pet.style.left - "px" + 100 + "px"
+    let horizontal = pet.style.left.replace(/\D/g,'')
+    pet.style.left = (Number(horizontal) + 50) + "px"
+  }
+  moveLeft(){
+    let pet = document.querySelector('#pet')
+    let horizontal = pet.style.left.replace(/\D/g,'')
+    pet.style.left = (Number(horizontal) - 50) + "px"
+  }
+  moveUp(){
+    let pet = document.querySelector('#pet')
+    let vertical = pet.style.bottom.replace(/\D/g,'')
+    pet.style.bottom = (Number(vertical) + 50) + "px"
+  }
+  moveDown(){
+    let pet = document.querySelector('#pet')
+    let vertical = pet.style.bottom.replace(/\D/g,'')
+    pet.style.bottom = (Number(vertical) - 50) + "px"
   }
 }
 
 const first = new Tamagotchi('Test')
 first.moveRight()
+first.moveDown()
+first.moveDown()
+first.moveLeft()
+first.moveLeft()
+first.moveUp()
 
 //WORKING MOVE RIGHT COMMAND
-const moveRight = () => {
-  let pet = document.querySelector('#pet')
-  console.log(pet)
-  horizontal = pet.style.left.replace(/\D/g,'')
-  console.log(horizontal)
-  pet.style.left = (Number(horizontal) + 10) + "px"
-}
+// const moveRight = () => {
+//   let pet = document.querySelector('#pet')
+//   horizontal = pet.style.left.replace(/\D/g,'')
+//   pet.style.left = (Number(horizontal) + 10) + "px"
+// }
